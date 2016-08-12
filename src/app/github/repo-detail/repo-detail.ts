6bbede0a6045cfd071fd02dc4ccbd1892a63aade
nadavsinai <ns@nadavsinai.com>
2016-08-12 21:37:20 +0300

    fix route and remove local state in repo details

--- src/app/github/repo-detail/repo-detail.ts
+++ src/app/github/repo-detail/repo-detail.ts
@@ -1,31 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {Github} from '../shared/github';
+import {Observable} from 'rxjs';
+
+import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/switchMap'
 
 @Component({
   selector: 'repo-detail',
   styleUrls: ['./repo-detail.css'],
   templateUrl: './repo-detail.html'
 })
-export class RepoDetail implements OnInit {
-  private org:string;
-  private repo:string;
-  public repoDetails:any = {};
+export class RepoDetail {
+  public repoDetails: Observable<any> = this.route.params
+    .map(params => ({org: this.route.parent.snapshot.params['org'], repo: params['repo'] || ''}))
+    .filter(params => params.repo)
+    .switchMap((params) => this.github.getRepoForOrg(params.org, params.repo));
 
-  constructor(public github:Github, private router:Router, private route:ActivatedRoute) {
+  constructor(public github: Github, private route: ActivatedRoute) {
   }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.org = this.router.routerState.parent(this.route).snapshot.params['org'];
-      this.repo = params['repo'] || '';
 
-      if (this.repo) {
-        this.github.getRepoForOrg(this.org, this.repo)
-          .subscribe(repoDetails => {
-            this.repoDetails = repoDetails;
-          });
-      }
-    });
-  }
 }
